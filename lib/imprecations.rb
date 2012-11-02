9372bd54669315963b3979ce9fb39bb245e0e5f7
Gabe Berke-Williams <gabe@thoughtbot.com>
2012-11-02 04:28:01 -0400

    Refactor

--- lib/imprecations.rb
+++ lib/imprecations.rb
@@ -7,48 +7,47 @@ module Imprecations
     end
 
     __imprecate(self)
-    self.imprecate_child_constants
+    imprecate_child_constants
   end
 
   def imprecate_child_constants
-    real_constants  = self.constants.map{|c| self.const_get(c) }
-    class_constants = real_constants.select{|c| Class === c }
-    class_constants.each do |class_const|
-      class_const.imprecate
-    end
+    real_constants = constants.map { |c| const_get(c) }
+    class_constants = real_constants.select { |c| Class === c }
+    class_constants.each(&:imprecate)
   end
 
+  def say(message)
+    puts message
+  end
+
+  private
+
   def __imprecate(constant)
-    unimprecated_instance_methods_for(constant).each do |meth|
-      namespaced_constant =
-        if self.superclass == Object
-          "#{constant}"
-        else
-          "#{self.superclass}::#{constant}"
-        end
+    unimprecated_instance_methods_for(constant).each do |method_name|
+      namespaced_constant = if superclass == Object
+                              constant.to_s
+                            else
+                              "#{superclass}::#{constant}"
+                            end
 
       constant.class_eval <<-EVIL
         # Store a reference
-        alias :"__unimprecated_#{meth}" :"#{meth}"
+        alias :"__unimprecated_#{method_name}" :"#{method_name}"
 
         # Undefine it so we can redefine it
-        undef_method(:"#{meth}")
+        undef_method "#{method_name}"
 
-        def #{meth}
-          Imprecations.say("DEPRECATION WARNING: #{namespaced_constant}##{meth} is deprecated!")
-          __unimprecated_#{meth}()
+        def #{method_name}
+          Imprecations.say("DEPRECATION WARNING: #{namespaced_constant}##{method_name} is deprecated!")
+          __unimprecated_#{method_name}()
         end
       EVIL
     end
   end
 
   def unimprecated_instance_methods_for(constant)
-    methods = constant.instance_methods(false)
-    methods.reject{|meth| meth.to_s =~ /^__unimprecated/ }
-  end
-
-  def say(message)
-    puts message
+    instance_methods_from_this_class= constant.instance_methods(false)
+    instance_methods_from_this_class.reject { |meth| meth.to_s =~ /^__unimprecated/ }
   end
 end
 
